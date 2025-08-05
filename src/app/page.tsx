5752c4f51e0a5d7c8bbc2a9845fc597880d6c002
Matheus Ramalho Silva <matheusramalhosilva2@gmail.com>
2025-08-04 21:34:24 -0300

    chore: add update shadcn theme, and formate import files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="p-4 bg-red-500 mx-5">
+    <div className="p-4">
       <Button> Botão shadcn </Button>
     </div>
   );
